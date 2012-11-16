3400bb039f962d8b265f2ec9beff20bfe39c90d7
Ben Beadle <benjbeadle@gmail.com>
2012-11-16 13:58:26 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponyplacementproject::Application.config.secret_token = 'cc379838d2521b84a20f1bcde4d53a04d3714ea914627f0eac8014a3f15d14954b210d5a5e7edceda2f5a660f1362a11222e872dea6d4fb5299ae4ccbe195a3c'
